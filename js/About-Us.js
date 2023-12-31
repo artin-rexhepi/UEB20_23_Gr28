240c2d89934f336e79978ce110addb8b7e5b87c5
Art Jashari <art.jashari1@student.uni-pr.edu>
2023-12-31 11:48:30 +0100

    Small adjustments
    Font correction, javascript additions to each page. Included back-to-top button.

--- js/About-Us.js
+++ js/About-Us.js
@@ -131,3 +131,34 @@ const reviewsData = [
 
         // Create team member cards
         teamMembers.forEach(createTeamMemberCard);
+
+
+        // -------------------------- Butoni back to top --------------------------
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Get the button
+    const mybutton = document.getElementById("backToTopBtn");
+
+    // When the user scrolls down 90px from the top of the document, show the button
+    window.onscroll = () => scrollFunction();
+
+    const scrollFunction = () => {
+        if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+            mybutton.style.display = "block";
+            mybutton.classList.add("show");
+        } else {
+            mybutton.classList.remove("show");
+        }
+    };
+
+    // When the user clicks on the button, scroll to the top of the document
+    mybutton.addEventListener("click", () => topFunction());
+
+    const topFunction = () => {
+        // Smooth scroll to the top
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    };
+});
--- js/FAQ.js
+++ js/FAQ.js
@@ -4,3 +4,34 @@ document.querySelectorAll('.question').forEach(function (question) {
                 answer.style.display = (answer.style.display === 'none' || answer.style.display === '') ? 'block' : 'none';
             });
         });
+
+
+// -------------------------- Butoni back to top --------------------------
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Get the button
+    const mybutton = document.getElementById("backToTopBtn");
+
+    // When the user scrolls down 90px from the top of the document, show the button
+    window.onscroll = () => scrollFunction();
+
+    const scrollFunction = () => {
+        if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+            mybutton.style.display = "block";
+            mybutton.classList.add("show");
+        } else {
+            mybutton.classList.remove("show");
+        }
+    };
+
+    // When the user clicks on the button, scroll to the top of the document
+    mybutton.addEventListener("click", () => topFunction());
+
+    const topFunction = () => {
+        // Smooth scroll to the top
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    };
+});
